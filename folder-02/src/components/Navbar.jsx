6e8dc592810e309b63451580546a2c776d7221ed
Deepesh Sharma <dikshitdipesh@gmail.com>
2026-03-08 01:01:02 +0530

    first commit

---
+++ folder-02/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='nav'>
+      <h1>Navbar</h1>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quasi deleniti mollitia nulla sequi deserunt quaerat, sunt, iure, sapiente quis quas corporis nihil dicta? Autem, veniam.</p>
+
+    </div>
+  )
+}
+
+export default Navbar
---
+++ folder-02/src/components/card.jsx
@@ -0,0 +1,9 @@
+function Card(){
+  return (
+    <div className='card'>
+      <h1>Deepesh Sharma</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae beatae accusantium laborum commodi. Adipisci, quod.</p>
+    </div>
+  )
+}
+export default Card;
