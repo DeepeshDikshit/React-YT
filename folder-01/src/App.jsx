00bc947ce4810de75d73f7dcb19707beef4d005a
Deepesh Sharma <dikshitdipesh@gmail.com>
2026-03-08 00:09:15 +0530

    first commit

---
+++ folder-01/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+    <div>
+      <h1>React App</h1>
+      <p>Welcome to your React app!</p>
+    </div>
+    <div>
+      <h2>Another Section</h2>
+      <p>This is another section of the app.</p>
+    </div>
+    </>
+  )
+}
+
+export default App
