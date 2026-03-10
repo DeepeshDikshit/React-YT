00f6b7c838df28fba90cdee43e29a8bb3abf49ff
Deepesh Sharma <dikshitdipesh@gmail.com>
2026-03-10 23:17:22 +0530

    04

---
+++ 04-cards-project/src/components/Card.jsx
@@ -0,0 +1,43 @@
+import { Bookmark } from 'lucide-react'
+import PropTypes from 'prop-types'
+
+const Card = (props) => {
+
+    return (
+        <div className="card">
+            <div>
+                <div className="top">
+                    <img src={props.brandLogo} alt="nachioooooo" />
+                    <button>Save <Bookmark size={10} /> </button>
+                </div>
+                <div className="center">
+                    <h3>{props.company} <span>{props.datePosted}</span></h3>
+                    <h2>{props.post}</h2>
+                    <div className='tag'>
+                        <h4>{props.tag1}</h4>
+                        <h4>{props.tag2}</h4>
+                    </div>
+                </div>
+            </div>
+            <div className="bottom">
+                <div>
+                    <h3>{props.pay}</h3>
+                    <p>Mumbai, India</p>
+                </div>
+                <button>Apply Now</button>
+            </div>
+        </div>
+    )
+}
+
+Card.propTypes = {
+    brandLogo: PropTypes.string.isRequired,
+    company: PropTypes.string.isRequired,
+    datePosted: PropTypes.string.isRequired,
+    post: PropTypes.string.isRequired,
+    tag1: PropTypes.string.isRequired,
+    tag2: PropTypes.string.isRequired,
+    pay: PropTypes.string.isRequired,
+}
+
+export default Card
