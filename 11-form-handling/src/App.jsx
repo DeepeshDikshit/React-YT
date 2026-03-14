8cf55e871e8a971a18a8c080fb16630760c44aea
Deepesh Sharma <dikshitdipesh@gmail.com>
2026-03-14 10:39:58 +0530

    initial change

---
+++ 11-form-handling/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const App = () => {
+  const handleSubmit = (e)=>{
+    e.preventDefault()
+    console.log("Form Submitted");
+  }
+  return (
+    <div>
+      <form onSubmit={(e)=>{
+        handleSubmit(e)
+      }} >
+        <input type="text" placeholder='Enter Your Name' />
+        <button>Submit</button>
+      </form>
+    </div>
+  )
+}
+
+export default App
